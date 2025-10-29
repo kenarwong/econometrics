0acf4c9fe5b54ea62877d8103d8522d76cd03fe7
Ken Hwang <ken.r.hwang.com>
2025-10-29 16:01:43 +0800

    1-5: Updated

--- 1-5/histogram.r
+++ 1-5/histogram.r
@@ -6,14 +6,17 @@ CSV = read_csv(r_File, col_names = TRUE)
 # Data = read.csv(r_File)
 
 Prices = CSV[,4]
-## Histogram of Prices
-## gspc
+# Histogram of Prices
+
+# gspc
+# Use bins of size 100, min = 3500, max = 7000
 Prices_GSPC = as.numeric(unlist(Prices[1:34,]))
-hist(Prices_GSPC, main="GSPC Price Distribution", xlab="Price", ylab="Frequency", col="lightblue", border="black")
+hist(Prices_GSPC, main="GSPC Price Distribution", xlab="Price", ylab="Frequency", col="lightblue", border="black", breaks=seq(3500, 7000, by=100))
 
 # msft
+# Use bins of size 10, min = 200, max = 550
 Prices_MSFT = as.numeric(unlist(Prices[35:68,]))
-hist(Prices_MSFT, main="MSFT Price Distribution", xlab="Price", ylab="Frequency", col="lightgreen", border="black")
+hist(Prices_MSFT, main="MSFT Price Distribution", xlab="Price", ylab="Frequency", col="lightgreen", border="black", breaks=seq(200, 550, by=10))
 
 # Frequency of different IDs
 # More useful for varying amounts of categorical data
